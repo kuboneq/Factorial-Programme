3e1a9a549f33695b282cf1002913deb1eea05288
kuboneq <117384544+kuboneq@users.noreply.github.com>
2022-11-10 19:27:07 +0100

    Update silnia.py

--- silnia.py
+++ silnia.py
@@ -11,17 +11,22 @@ nums_after  = list()
 
 print("Obliczanie silni ")
 while True:
-    user_input = input("Podaj liczbe z ktorej chcesz obliczyc silnie lub Q, by wyjsc ")
-    if user_input.lower() == 'q':
-        print("Podsumowanie dzialania programu:\n")
-        
-        for num, tuple_num in enumerate(zip(nums_before, nums_after)):
-            print("[Nr.{}] Wartosc: {}, Silnia: {}.".format(num +1, tuple_num[0], tuple_num[1]))
-        exit()
+    user_input = input("Podaj liczbe z ktorej chcesz obliczyc silnie lub Q, by wyjsc: ")
+    for i in user_input:
+        if i not in '0123456789qQ':
+            print('Podaj poprawny numer')
+            break
     else:
-        result = silnia(int(user_input))
-        print("Silnia z {}, wynosi {}".format(int(user_input),result))
-        nums_before.append(user_input)
-        nums_after.append(result)
+        if user_input.lower() == 'q':
+            print("Podsumowanie dzialania programu:\n")
+            
+            for num, tuple_num in enumerate(zip(nums_before, nums_after)):
+                print("[Nr.{}] Wartosc: {}, Silnia: {}.".format(num +1, tuple_num[0], tuple_num[1]))
+            exit()
+        else:
+            result = silnia(int(user_input))
+            print("Silnia z {}, wynosi {}".format(int(user_input),result))
+            nums_before.append(user_input)
+            nums_after.append(result)
 
 
